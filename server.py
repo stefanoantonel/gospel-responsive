90477090efc54dd98c44efeb05ce27bb2c07413e
hp <stefanoantonel@gmail.com>
2015-11-04 22:06:22 -0300

    Finished

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__, static_url_path='/views')
-
-@app.route("/")
-def hello():
-    return app.send_static_file('/index.html')
-
-if __name__ == "__main__":
-    app.run()
