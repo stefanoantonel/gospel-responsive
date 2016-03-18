059cf92d53a80c3afa9c40c40f8b24adad228bce
hp <stefanoantonel@gmail.com>
2016-03-18 11:27:41 -0300

    Upgrading dependences

--- server.js
+++ server.js
@@ -3,44 +3,192 @@
 var express = require('express');
 var fs      = require('fs');
 var request = require('request');
-var Xray = require('x-ray');
+var Xray    = require('x-ray');
 var favicon = require('serve-favicon');
-var x = Xray()
-var path = require('path');
-var Gospel = {
+var x       = Xray()
+var path    = require('path');
+var Gospel  = {
     text: ''
 }
 
-var express = require("express");
-var parser = require("body-parser");
-var fs = require('fs');
-var web = express();
-var request = require('request');
-var port = 8080;
+/**
+ *  Define the sample application.
+ */
+var SampleApp = function() {
 
-var app = express();
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(express.static('public'));
-app.set('view engine', 'jade');
-
-var server = app.listen(port, function() {
-	console.log("Server just started in port: " + port);
-});
+    //  Scope.
+    var self = this;
 
 
-app.get("/", function(req, res) {
-	res.render('index', { 
-		title: 'Gospel', 
-		message: "Loading..."
-	});
-});
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
+
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
 
 
-app.get('/getGospel', function(req, res) {
-	x('http://www.ciudadredonda.org/calendario-lecturas/evangelio-del-dia/hoy', ['div.texto_palabra']) 
-	(function(err, parts) {
-		var text = parts[parts.length -1];
-		Gospel.text = text;
-		res.send(Gospel.text);
-	});
-});
+    /**
+     *  Populate the cache.
+     */
+    self.populateCache = function() {
+        if (typeof self.zcache === "undefined") {
+            self.zcache = { 'index.html': '' };
+        }
+
+        //  Local cache for static content.
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    };
+
+
+    /**
+     *  Retrieve entry (content) from cache.
+     *  @param {string} key  Key identifying content to retrieve from cache.
+     */
+    self.cache_get = function(key) { return self.zcache[key]; };
+
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function() {
+        
+        
+        self.routes = { };
+
+        self.routes['/asciimo'] = function(req, res) {
+            var link = "http://i.imgur.com/kmbjB.png";
+            res.send("<html><body><img src='" + link + "'></body></html>");
+        };
+
+        self.routes['/a'] = function(req, res) {
+            var link = 'http://www.iconsdb.com/icons/preview/royal-blue/holy-bible-xl.png';
+            console.log("SRC: " + link);
+            res.send("<html><body><img src='" + link + "'></body></html>");
+        };
+
+        /*self.routes['/'] = function(req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(self.cache_get('index.html') );
+        };*/
+
+        self.routes['/'] = function(req, res) {
+            res.render('index', { 
+                title: 'Gospel', 
+                message: "Loading..."
+            });
+        };
+
+        self.routes['/getGospel'] = function(req, res) {
+            x('http://www.ciudadredonda.org/calendario-lecturas/evangelio-del-dia/hoy', ['div.texto_palabra']) 
+            (function(err, parts) {
+                var text = parts[parts.length -1];
+                Gospel.text = text;
+                res.send(Gospel.text);
+            });
+        };
+
+    };
+
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express();
+        //self.app.use(favicon(__dirname + '/public/favicon.ico'));
+        self.app.use(express.static('public'));
+        self.app.set('view engine', 'jade');
+        
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
+
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        self.populateCache();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};   /*  Sample Application.  */
+
+
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
+
