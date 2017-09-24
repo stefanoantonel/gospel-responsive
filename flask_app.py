cbf09b3b1f93bae141142e4b92770b687948c5ad
stefanoantonel <stefanoantonel@users.noreply.github.com>
2017-09-24 03:05:10 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,3 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
 import feedparser
 
@@ -14,8 +11,9 @@ def hello_world():
     gospel = get_gospel(evang_value)
     audio = get_audio(evang_value)
     copyright = get_copyright()
-    content = complete_html(gospel + audio + copyright)
+    content = gospel_html(gospel + audio + copyright)
     return content
+
 
 def find_gospel():
     d = feedparser.parse('http://feeds.feedburner.com/mdc-misionerosdigitales')
@@ -33,15 +31,19 @@ def find_gospel():
         i = i + 1
     return gospel
 
+
 def get_gospel(evang_value):
     index_evang = evang_value.find(search_word)
-    end_evang = evang_value.find("<p style", index_evang)
+    print (index_evang)
+    end_evang = evang_value.find("</p>", index_evang)
     return evang_value[index_evang:end_evang]
+
 
 def get_audio(evang_value):
     index_audio = evang_value.find('<audio')
     end_audio = evang_value.find('</audio>', index_audio)
     return evang_value[index_audio:end_audio+8] # to capture the tag
+
 
 def get_style():
     return '''<style>
@@ -56,10 +58,13 @@ def get_style():
             float: right;
         }
     </style>'''
+
+
 def get_copyright():
     return '<p class="copyright">Contenido extraido de misioneros digitales</p>'
 
-def complete_html(content):
+
+def gospel_html(content):
     return '''<html>
         <head><title>Evangelio</title><meta name="viewport" ,="" content="width=device-width, initial-scale=1">
             <meta charset="utf-8">
@@ -76,6 +81,8 @@ def complete_html(content):
                     color: white;
                     font-family: sans-serif;
                     line-height: 1.5;
+                    max-width: 650px;
+                    margin: 10px auto;
                 }
                 .copyright {
                     font-size: 8px;
