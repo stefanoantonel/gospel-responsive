8db2cea4bc3453d2fc42ed62a7622c317c9eeff7
stefanoantonel <stefanoantonel@users.noreply.github.com>
2021-12-05 20:29:29 +0100

    Update main.yml (#4)
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * Update run.py
    
    * Update main.yml
    
    * Update main.yml

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import gospel_utils
-
+from pathlib import Path
 
 evang_value = gospel_utils.find_gospel()
 gospel = gospel_utils.get_gospel(evang_value)
@@ -7,5 +7,7 @@ audio = gospel_utils.get_audio(evang_value)
 copyright = gospel_utils.get_copyright()
 content = gospel_utils.gospel_html(gospel + audio + copyright)
 
-with open('./index.html', 'w') as f:
-  f.writelines(content)
+output_path = Path('./dist')
+output_path.mkdir(parents=True, exist_ok=True)
+with open(output_path / 'index.html', 'w') as f:
+    f.writelines(content)
